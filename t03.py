510043e3f516611b66dc82360e91e7c80e3f5614
97612336 <xkjskk@163.com>
2018-03-09 10:53:48 +0800

    .

--- t03.py
+++ t03.py
@@ -4,4 +4,28 @@
 # print('asd{}'.format((1,2,3,4,5)))
 
 
-print(enumerate((1,23,45,6)))
+# print(enumerate((1,23,45,6)))
+import celery
+
+
+def show(fn):
+    def wrapper():
+        return fn()+123
+    return wrapper
+
+@show
+def go():
+    print('nihao')
+    return 123
+
+# print(go())
+
+
+app=celery.Celery('t03',broker='redis://127.0.0.1')
+
+@app.task 
+def go_test():
+    for i in range(10):
+        print(i)
+
+go_test()
